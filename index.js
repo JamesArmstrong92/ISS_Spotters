fb266f20a9eaee36dce0aee4fded1435db0bab28
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-11 17:36:22 -0500

    Removed unnessessary code

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
-
-
 const { nextISSTimesForMyLocation } = require('./iss');
 
 nextISSTimesForMyLocation((error, passTimes) => {
